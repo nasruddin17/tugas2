511fa78616e42e8994d06f93d8edad80959b06dc
Nasruddin <43714390+nasruddin17@users.noreply.github.com>
2018-12-11 22:16:56 +0800

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,31 @@
+public class Node {
+
+	private String label;
+
+	private int value;
+
+	public String getLabel() {
+
+		return label;
+
+	}
+
+	public int getValue() {
+
+		return value;
+
+	}
+
+	public void setLabel(String label) {
+
+		this.label = label;
+
+	}
+
+	public void setValue(int value) {
+
+		this.value = value;
+
+	}
+
+}
