cf3ed98af33a947a23b7ca17614a9a8d2ce2a4e1
Nasruddin <43714390+nasruddin17@users.noreply.github.com>
2018-12-11 22:14:59 +0800

    Create Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,91 @@
+public class Mahasiswa {
+
+	private String nama, nim, alamat, golongandarah, status;
+
+	private double tinggibadan, beratbadan;
+
+	public String getNama() {
+
+		return nama;
+
+	}
+
+	public String getNim() {
+
+		return nim;
+
+	}
+
+	public String getAlamat() {
+
+		return alamat;
+
+	}
+
+	public String getGolongandarah() {
+
+		return golongandarah;
+
+	}
+
+	public String getStatus() {
+
+		return status;
+
+	}
+
+	public double getTinggibadan() {
+
+		return tinggibadan;
+
+	}
+
+	public double getBeratbadan() {
+
+		return beratbadan;
+
+	}
+
+	public void setNama(String nama) {
+
+		this.nama = nama;
+
+	}
+
+	public void setNim(String nim) {
+		this.nim = nim;
+
+	}
+
+	public void setAlamat(String alamat) {
+
+		this.alamat = alamat;
+
+	}
+
+	public void setGolongandarah(String golongandarah) {
+
+		this.golongandarah = golongandarah;
+
+	}
+
+	public void setStatus(String status) {
+
+		this.status = status;
+
+	}
+
+	public void setTinggibadan(double tinggibadan) {
+
+		this.tinggibadan = tinggibadan;
+
+	}
+
+	public void setBeratbadan(double beratbadan) {
+
+		this.beratbadan = beratbadan;
+
+	}
+
+
+}
