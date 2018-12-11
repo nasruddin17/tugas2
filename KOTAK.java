cdc233306305ce45182726e433277bc6b1a97a19
Nasruddin <43714390+nasruddin17@users.noreply.github.com>
2018-12-11 22:13:08 +0800

    Create KOTAK.java

---
+++ KOTAK.java
@@ -0,0 +1,65 @@
+public class KOTAK {
+
+	private double panjang, lebar, luas, keliling;
+
+
+	public double getPanjang() {
+
+		return panjang;
+
+	}
+
+
+	public double getLebar() {
+
+return lebar
+
+	}
+
+
+	public double getLuas() {
+
+		luas = panjang * lebar;
+
+		return luas;
+
+	}
+
+
+	public double getKeliling() {
+
+		keliling = panjang + lebar + panjang + lebar;
+
+		return keliling; 
+
+	}
+
+
+	public void setPanjang(double panjang) {
+
+		this.panjang = panjang;
+
+	}
+
+
+	public void setLebar(double lebar) {
+
+		this.lebar = lebar;
+
+	}
+
+
+	public void setLuas(double luas) {
+
+		this.luas = luas;
+
+	}
+
+
+	public void setKeliling(double keliling) {
+
+		this.keliling = keliling;
+
+	}
+
+}
